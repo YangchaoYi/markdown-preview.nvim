e0484dafd35fb5a6bfc1a5d58a0013674f9b6a8c
YangchaoYi <wilomit@outlook.com>
2022-04-17 13:38:06 -0400

    change some console.log to myConsole.log

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,9 @@
 import load from './load'
+import fs from 'fs';
 
 const PATH = '--path'
 const VERSION = '--version'
+const myConsole = new console.Console(fs.createWriteStream('/home/yangchao/mkdp_ycyi.log'));
 
 const { argv = [] }: { argv: string[] } = process
 
@@ -9,6 +11,7 @@ const param = argv[2]
 
 if (param === PATH) {
   load(argv[3]).run()
+  myConsole.log(argv[3]);
 } else if (param === VERSION) {
   // tslint:disable-next-line
   console.log('0.0.9')
